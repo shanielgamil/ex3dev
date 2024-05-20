c5f1bfaf1d80dcd3402f6927fed4a0514e2eacca
shanielgamil <elgamilshani@gmail.com>
2024-05-20 15:54:47 +0300

    change3

--- shani.java
+++ shani.java
@@ -2,7 +2,7 @@ public class shani {
     public static void main(String[] args) {
      
         for (int i = 1; i <= 10; i++) {
-          System.out.println(" aaaaaaaaaaaaaaa");
+          System.out.println("daniel dolberg");
         }
        
     }
